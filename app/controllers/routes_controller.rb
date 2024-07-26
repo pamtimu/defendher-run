72b44d5e4e0609765289c953b723e7450242f9f2
pamera timu <pamerapaasi@gmail.com>
2024-07-26 11:36:21 +0200

    changed the routes controller to strong params

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,11 +1,20 @@
 class RoutesController < ApplicationController
+before_action :set_route, only: [:show]
 
   def index
     @routes = Route.all
   end
 
   def show
-    @route = Route.find(params[:id])
   end
 
+  private
+
+  def route_params
+    params.require(:route).permit(:name, :description)
+  end
+
+  def set_route
+    @route = Route.find(params[:id])
+  end
 end
