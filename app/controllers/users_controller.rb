52b5e80a12031aa6242f9269ce0f2419fb4f4bae
christian carbonari <christiancarbonari9@gmail.com>
2024-07-27 15:34:38 +1000

    users show page controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show]
+
   def show
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:id)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
