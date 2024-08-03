f77e71a067774ccc777400f8b93c49968382573d
Andrew Walton <andrewjamesnomad@gmail.com>
2024-08-03 11:53:51 +1000

    linked updates button to updates page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,9 @@ class PagesController < ApplicationController
   def home
   end
 
+  def updates
+  end
+
   def saved_routes
     # @saved_routes = SavedRoute.where(user_id: current_user.id)
     @routes = current_user.routes
