4a22d079837aa9a49857bc505b5039edd65e6716
christian carbonari <christiancarbonari9@gmail.com>
2024-08-03 15:07:22 +1000

    chatroom create action created

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -5,9 +5,18 @@ class ChatroomsController < ApplicationController
   end
 
   def create
-    @chatroom = Chatroom.new
-    @chatroom.save
-    redirect_to chatroom_path(@chatroom)
+    if chatroom_exist?
+      redirect_to chatroom_path(@chatroom)
+    else
+      @chatroom = Chatroom.new
+      @chatroom.user_one = current_user
+      @chatroom.user_two = User.find(params[:user_id])
+      if @chatroom.save
+        redirect_to chatroom_path(@chatroom)
+      else
+        redirect_to user_path(current_user)
+      end
+    end
   end
 
   def index
@@ -18,5 +27,11 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @chatroom.destroy
     redirect_to chatrooms_path
+  end
+
+  private
+
+  def chatroom_exist?
+    @chatroom = Chatroom.find_by(user_one: current_user, user_two: User.find(params[:user_id]))
   end
 end
