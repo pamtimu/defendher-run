e5b5a7e714f6b76e6861170ecbbe72c60d803a3f
christian carbonari <christiancarbonari9@gmail.com>
2024-08-03 10:46:14 +1000

    CRUD actions for chatrooms

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -3,4 +3,20 @@ class ChatroomsController < ApplicationController
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
   end
+
+  def create
+    @chatroom = Chatroom.new
+    @chatroom.save
+    redirect_to chatroom_path(@chatroom)
+  end
+
+  def index
+    @chatrooms = Chatroom.all
+  end
+
+  def destroy
+    @chatroom = Chatroom.find(params[:id])
+    @chatroom.destroy
+    redirect_to chatrooms_path
+  end
 end
