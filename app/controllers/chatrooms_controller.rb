e12c09694083f1706b09c5e156374b1ef8cc5fb7
christian carbonari <christiancarbonari9@gmail.com>
2024-08-06 19:41:45 +1000

    delete button added in the chatroom show page

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,6 +2,7 @@ class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+    @user = @chatroom.user_two
   end
 
   def create
