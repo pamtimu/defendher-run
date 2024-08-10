b7c231ed28fe2cf9bce5287ce74527c80c2e3c81
Andrew Walton <andrewjamesnomad@gmail.com>
2024-08-10 10:57:24 +1000

    back-end created for notifications page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,21 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
-
+  
   def home
   end
 
   def updates
+    @chatrooms = Chatroom.where(user_one: current_user) + Chatroom.where(user_two: current_user)
+    @messages = @chatrooms.map do |chatroom|
+      chatroom.messages.order(created_at: :desc).first
+    end
+      @friendships = Friendship.where(user_one: current_user)
+      @friendships.map do |friendship|
+    end
   end
 
   def saved_routes
     # @saved_routes = SavedRoute.where(user_id: current_user.id)
     @routes = current_user.routes
   end
-
 end
