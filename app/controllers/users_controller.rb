eb981bcf8f8349ab804986be2f1f882ca6327bef
christian carbonari <christiancarbonari9@gmail.com>
2024-08-15 16:33:57 +0700

    added method to modify biography

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,18 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show]
+  before_action :set_user, only: [:show, :update_biography]
 
   def show
+  end
+
+  def update_biography
+    @user.update(user_params)
+    redirect_to user_path(@user)
   end
 
   private
 
   def user_params
-    params.require(:user).permit(:id, :photo)
+    params.require(:user).permit(:id, :photo, :biography)
   end
 
   def set_user
