1ba4e03902fae5ee6656d495220d5cd4b382f205
christian carbonari <christiancarbonari9@gmail.com>
2024-08-17 11:31:07 +0700

    searchbar feature

--- app/models/route.rb
+++ app/models/route.rb
@@ -6,4 +6,11 @@ class Route < ApplicationRecord
   geocoded_by :address
   #has_one_attached :image
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_by_name_and_address,
+    against: [ :name, :address ],
+    using: {
+      tsearch: { prefix: true }
+    }
 end
