d7a87feb8e4038935a89b00d882f5bfa48d475ec
pamera timu <pamerapaasi@gmail.com>
2024-08-17 02:55:41 +0200

    changed the height and width of navbar and position, also for the footer

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [ :home ]
-  
+
   def home
   end
 
@@ -16,5 +16,16 @@ class PagesController < ApplicationController
 
   def saved_routes
     @routes = current_user.routes
+  end
+
+  def suggested_friends
+    @friendships = Friendship.where(user_one: current_user) + Friendship.where(user_two: current_user)
+    @friendships.map do |friendship|
+      if friendship.user_one == current_user
+        @friend = friendship.user_two
+      else
+        @friend = friendship.user_one
+      end
+    end
   end
 end
