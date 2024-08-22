9310f7d6851e9ba8ae438804b9fa4d8b45ef7aff
pamera timu <pamerapaasi@gmail.com>
2024-08-22 13:12:26 +0200

    allow no profile pages to still show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -21,11 +21,11 @@ class PagesController < ApplicationController
     @messages = @chatrooms.map do |chatroom|
       chatroom.messages.includes(:chatroom).order(created_at: :desc).first
     end.compact
+
     @friendships = Friendship.where(user_two: current_user, accepted: "pending")
     @updates = (@messages + @friendships).sort_by { |update| update.created_at }.reverse
 
     @unseen_messages = @messages.select { |message| message.seen == false && message.user.id != current_user.id }
-    
   end
 
   def saved_routes
